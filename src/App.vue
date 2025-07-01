60542590dd0b6ea29a8b2fccb48dca7fdb9074ea
Yusuf Arda Peker <yusufardapkr@gmail.com>
2025-07-01 17:21:09 +0300

    Add hero

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Header from "./components/header/Header.vue";
+import Hero from "./components/Hero.vue";
 </script>
 
 <template>
 	<Header />
+	<Hero />
 </template>
---
+++ src/components/Hero.vue
@@ -0,0 +1,19 @@
+<script setup>
+import heroIllsutration from "@/images/illustration-working.svg";
+import Button from "./shared/Button.vue";
+</script>
+
+<template>
+	<div id="hero">
+		<img :src="heroIllsutration" class="hero-img" alt="working-illustration" />
+
+		<div class="text-content container">
+			<h1>More than just shorter links</h1>
+			<p class="hero-description">
+				Build your brand's recognition and get detailed insights on how your links are performing.
+			</p>
+
+			<Button>Get Started</Button>
+		</div>
+	</div>
+</template>
