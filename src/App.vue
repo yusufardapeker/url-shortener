c4bab4bd2fc0833a59739851dee4f0d0d487fbea
Yusuf Arda Peker <yusufardapkr@gmail.com>
2025-07-09 17:04:30 +0300

    Add get started component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Header from "./components/header/Header.vue";
 import Hero from "./components/Hero.vue";
 import UrlShorter from "./components/UrlShorter.vue";
 import Features from "./components/features/Features.vue";
+import GetStarted from "./components/GetStarted.vue";
 </script>
 
 <template>
@@ -10,4 +11,5 @@ import Features from "./components/features/Features.vue";
 	<Hero />
 	<UrlShorter />
 	<Features />
+	<GetStarted />
 </template>
---
+++ src/components/GetStarted.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Button from "./shared/Button.vue";
+import boostBgMobile from "@/images/bg-boost-mobile.svg";
+</script>
+
+<template>
+	<section id="get-started">
+		<img :src="boostBgMobile" alt="background-img" class="bg-img" />
+
+		<h2 class="title">Boost your links today</h2>
+		<Button>Get Started</Button>
+	</section>
+</template>
